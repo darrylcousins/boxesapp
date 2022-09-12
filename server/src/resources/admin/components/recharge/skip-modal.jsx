ee9bab1f26f9259286ca8741b4c613c621f5f5f9
Darryl Cousins <darryljcousins@gmail.com>
2022-09-12 20:14:55 +1200

    Allow customer to select pause period.
    Being 1-3 subscription intervals.

--- server/src/resources/admin/components/recharge/skip-modal.jsx
+++ server/src/resources/admin/components/recharge/skip-modal.jsx
@@ -11,6 +11,7 @@ import { createElement, Fragment } from "@b9g/crank";
 import Button from "../lib/button";
 import FormModalWrapper from "../form/form-modal";
 import Form from "../form";
+import { animateFadeForAction } from "../helpers";
 
 /**
  * Icon component for link to expand modal
@@ -42,7 +43,7 @@ const options = {
   id: "pause-subscription", // form id - matches name in ShowLink which is title.toHandle
   title: "Pause Subscription",
   color: "dark-red",
-  src: "/api/recharge-skip-charge",
+  src: "/api/recharge-update-charge-date",
   ShowLink,
   saveMsg: "Pausing subscription ...",
   successMsg: "Successfully paused subscription, reloading page.",
@@ -57,48 +58,114 @@ const options = {
  * @param {Function} props.doSave - The save action
  * @param {Function} props.closeModal - The cancel and close modal action
  * @param {string} props.title - Form title
- * @param {object} props.box - The box to be removed
+ * @param {object} props.subscription - The subscription to be paused
  * @param {string} props.formId - The unique form indentifier
  */
 async function* SkipCharge(props) {
   const { doSave, closeModal, title, subscription, formId } = props;
+
+  const deliveryDays = [];
+  const chargeDays = [];
+  const intervalDays = [];
+  const interval = subscription.attributes.days === 7 ? "week" : "fortnigt";
+  const multiplier = subscription.attributes.days === 7 ? 1 : 2;
+  let delivered = new Date(Date.parse(subscription.attributes.nextDeliveryDate));
+  let charge = new Date(Date.parse(subscription.attributes.nextChargeDate));
+  for (let i=0; i<3; i++) {
+    intervalDays.push(`${multiplier * (i + 1)} week${i > 0 ? "s" : ""}`);
+    delivered.setDate(delivered.getDate() + subscription.attributes.days);
+    deliveryDays.push(delivered.toDateString());
+    charge.setDate(charge.getDate() + subscription.attributes.days);
+    chargeDays.push(charge.toDateString());
+  };
+
+  let intervalIndex = 0;
+
+  /*
+   * Updates on box selection
+   * Load dates available for the selected box
+   */
+  const onIntervalChange = async (ev) => {
+    intervalIndex = intervalDays.indexOf(ev.target.value);
+    const chargeEl = document.getElementById("charge-date");
+    const deliveryEl = document.getElementById("delivery-date");
+    animateFadeForAction(chargeEl, () => chargeEl.innerHTML = chargeDays[intervalIndex]);
+    animateFadeForAction(deliveryEl, () => deliveryEl.innerHTML = deliveryDays[intervalIndex]);
+    document.getElementById("nextchargedate").value = chargeDays[intervalIndex];
+    document.getElementById("nextdeliverydate").value = deliveryDays[intervalIndex];
+  };
+
+  /*
+   * Updates on box selection
+   */
+  const onSelect = async (ev) => {
+    //const selectEl = document.getElementById("pauseinterval");
+    ev.target.classList.add("mb3");
+  };
+
+  /**
+   * The initial data of the form
+   *
+   * @function getInitialData
+   * @returns {object} The initial data for the form
+   * returns the box else compiles reasonable defaults.
+   */
+  const getInitialData = () => {
+    const data = {
+      attributes: JSON.stringify(subscription.attributes),
+      includes: JSON.stringify(subscription.includes),
+      pauseinterval: intervalDays[intervalIndex],
+      nextchargedate: chargeDays[intervalIndex],
+      nextdeliverydate: deliveryDays[intervalIndex],
+    };
+    return data;
+  };
 
   /**
    * The form fields - required by {@link module:app/form/form~Form|Form}.
    *
    * @member {object} fields The form fields keyed by field title string
    */
-  const fields = {
-    attributes: {
-      type: "hidden",
-      datatype: "string",
-    },
-    includes: {
-      type: "hidden",
-      datatype: "string",
-    },
+  const getFields = () => {
+
+    return {
+      attributes: {
+        type: "hidden",
+        datatype: "string",
+      },
+      includes: {
+        type: "hidden",
+        datatype: "string",
+      },
+      nextChargeDate: {
+        type: "hidden",
+        datatype: "string",
+      },
+      nextDeliveryDate: {
+        type: "hidden",
+        datatype: "string",
+      },
+      pauseInterval: {
+        label: "Select Pause Interval",
+        type: "input-select",
+        size: "50",
+        datatype: "string",
+        datalist: intervalDays,
+        required: true,
+        onchange: onIntervalChange,
+        onclick: onSelect,
+      },
+    };
   };
 
   for await (const _ of this) { // eslint-disable-line no-unused-vars
-
-    /**
-     * The initial data of the form
-     *
-     * @function getInitialData
-     * @returns {object} The initial data for the form
-     * returns the box else compiles reasonable defaults.
-     */
-    const getInitialData = () => ({
-      attributes: JSON.stringify(subscription.attributes),
-      includes: JSON.stringify(subscription.includes),
-    });
 
     /*
      * Data passed to form to create the toast message to user on doSave of form
      * These values can be arbitary provided that match the template string
      */
     const toastTemplate = {
-      template: "${title} - ${variant} subscription skipped successfully.",
+      template: "${title} - ${variant} subscription paused successfully.",
       title: subscription.box.shopify_title,
       variant: subscription.attributes.variant,
     };
@@ -112,49 +179,51 @@ async function* SkipCharge(props) {
     const updatedCharge = chargeObj.toDateString();
 
     yield (
-      <Fragment>
+      <div class="w-80 center">
         <p class="lh-copy tl">
           Are you sure you want to pause the subscription?<br />
           <b class="pt3">This cannot be undone.</b>
           <div class="cf">
-            <div class="fl w-50 gray tr pr3 pv1 b">
-              This charge date:
-            </div>
-            <div class="fl w-50 pv1 b">
-              { subscription.attributes.nextChargeDate }
-            </div>
             <div class="fl w-50 gray tr pr3 pv1 b">
               Scheduled delivery date:
             </div>
             <div class="fl w-50 pv1 b">
               { subscription.attributes.nextDeliveryDate }
             </div>
+            <div class="fl w-50 gray tr pr3 pv1 b">
+              This charge date:
+            </div>
+            <div class="fl w-50 pv1 b">
+              { subscription.attributes.nextChargeDate }
+            </div>
           </div>
         </p>
         <p class="lh-copy tl">
-          <div class="cf">
-            <div class="fl w-50 gray tr pr3 pv1 b">
-              New charge date will be:
-            </div>
-            <div class="fl w-50 pv1 b">
-              { updatedCharge }
-            </div>
+          <div class="cf mt3">
             <div class="fl w-50 gray tr pr3 pv1 b">
               New delivery date will be:
             </div>
-            <div class="fl w-50 pv1 b">
-              { updatedDelivery }
+            <div class="fl w-50 pv1 b" id="delivery-date">
+              { deliveryDays[intervalIndex] }
+            </div>
+            <div class="fl w-50 gray tr pr3 pv1 b">
+              New charge date will be:
+            </div>
+            <div class="fl w-50 pv1 b" id="charge-date">
+              { chargeDays[intervalIndex] }
             </div>
           </div>
         </p>
-        <Form
-          data={getInitialData()}
-          fields={fields}
-          title={title}
-          id={formId}
-          meta={toastTemplate}
-        />
-        <div class="tr">
+        <div class="w-100 pl7 mb3">
+          <Form
+            data={getInitialData()}
+            fields={getFields()}
+            title={title}
+            id={formId}
+            meta={toastTemplate}
+          />
+        </div>
+        <div class="cf tr">
           <Button type="primary" onclick={doSave}>
             Yes, Pause Subscription
           </Button>
@@ -162,7 +231,7 @@ async function* SkipCharge(props) {
             Cancel
           </Button>
         </div>
-      </Fragment>
+      </div>
     );
   }
 }

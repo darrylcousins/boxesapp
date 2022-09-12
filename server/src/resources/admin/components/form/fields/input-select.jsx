e195e496e716f5e3bac2bcf0c22f2128887851bf
Darryl Cousins <darryljcousins@gmail.com>
2022-09-12 20:10:43 +1200

    More padding.

--- server/src/resources/admin/components/form/fields/input-select.jsx
+++ server/src/resources/admin/components/form/fields/input-select.jsx
@@ -90,7 +90,7 @@ select {
         </style>
       <FieldWrapper label={label} size={size} id={id} hideLabel={hideLabel}>
         <select
-          class={`mr1 pa2 ba bg-transparent hover-bg-near-white w-100 input-reset br2 ${
+          class={`mr1 pa2 pl3 ba bg-transparent hover-bg-near-white w-100 input-reset br2 ${
             !valid ? "invalid" : ""
           }`}
           disabled={disabled}

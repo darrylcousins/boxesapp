61bdb67313a61f3c69e59f4f5881f17e8ba501a2
Darryl Cousins <darryljcousins@gmail.com>
2022-09-22 17:51:25 +1200

    Additional cid check. Superfluous maybe.

--- server/src/resources/customer/components/home.jsx
+++ server/src/resources/customer/components/home.jsx
@@ -4,6 +4,10 @@ import Customer from "../../../resources/admin/components/recharge/customer";
 export default async function Home() {
   const customerJson = document.querySelector("#customer-json");
   const customer = await JSON.parse(customerJson.textContent);
-  console.log(customer.id);
+  const cid = document.getElementById('initialize').getAttribute('cid');
+  if (cid !== customer.id) {
+    window.location.assign(window.location.href.split('?')[0]);
+  };
+
   return <Customer customer={ customer } admin={ false } /> ;
 };

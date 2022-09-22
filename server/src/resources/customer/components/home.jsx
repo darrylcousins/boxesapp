9f006a89f7f0b18b3729bebd1b82958ce8e5c1f1
Darryl Cousins <darryljcousins@gmail.com>
2022-09-22 21:02:50 +1200

    Reversing a trial -failed on other server.

--- server/src/resources/customer/components/home.jsx
+++ server/src/resources/customer/components/home.jsx
@@ -4,10 +4,12 @@ import Customer from "../../../resources/admin/components/recharge/customer";
 export default async function Home() {
   const customerJson = document.querySelector("#customer-json");
   const customer = await JSON.parse(customerJson.textContent);
+  /*
   const cid = document.getElementById('initialize').getAttribute('cid');
   if (cid !== customer.id) {
     window.location.assign(window.location.href.split('?')[0]);
   };
+  */
 
   return <Customer customer={ customer } admin={ false } /> ;
 };

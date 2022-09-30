4d03b9c4aed8b7fc1ef1334b8681779dc609a6e7
Darryl Cousins <darryljcousins@gmail.com>
2022-09-30 13:29:16 +1300

    Better checking when adding product.
    Now checking for correct tags, options, and selling plans.

--- server/src/api/box/add-product-to-box.js
+++ server/src/api/box/add-product-to-box.js
@@ -3,7 +3,9 @@
  * @author Darryl Cousins <darryljcousins@gmail.com>
  */
 
+import path from "path";
 import { makeShopQuery } from "../../lib/shopify/helpers.js";
+import { queryStoreGraphQL } from "../../lib/shopify/helpers.js";
 import { ObjectID } from "mongodb";
 
 /*
@@ -22,52 +24,77 @@ export default async (req, res, next) => {
     return;
   };
 
-  // first collect the product details from shopify
-  const path = "products.json";
-  const fields = ["id", "title", "handle", "variants", "tags"];
-  const limit = 3;
-  const query = [
-    ["ids", shopify_product_id.toString()]
-  ];
+  const body = `{
+    product (id: "gid://shopify/Product/${shopify_product_id}") {
+      id
+      handle
+      tags
+      title
+      sellingPlanGroupCount
+      productType
+      sellingPlanGroups (first: 1) {
+        nodes {
+          options
+        }
+      }
+      variants (first: 2) {
+        nodes {
+          id
+          price
+          title
+          displayName
+        }
+      }
+    }
+  }`;
 
   // get settings tags
   const tagQuery = await _mongodb.collection("settings").find({handle: "product-tags"}).toArray();
   const tags = tagQuery[0].value;
 
   const makeDoc = (product) => {
-    const tag = product.tags.split(',').map(el => el.trim()).find(el => tags.includes(el));
     return {
       _id: new ObjectID(),
       shopify_title: product.title.replace(/,/g, ""),
       shopify_handle: product.handle,
-      shopify_product_id: product.id,
-      shopify_variant_id: product.variants[0].id,
-      shopify_price: parseFloat(product.variants[0].price) * 100,
-      shopify_tag: tag
+      shopify_product_id,
+      shopify_variant_id: path.basename(product.variants.nodes[0].id),
+      shopify_price: parseFloat(product.variants.nodes[0].price) * 100,
+      shopify_tag: product.tag
     }
   };
 
-  const productDoc = await makeShopQuery({path, limit, query, fields})
+  const productDoc = await queryStoreGraphQL({ body })
     .then(async (res) => {
-      if (!Object.hasOwnProperty.call(res, "products")) {
+      if (!Object.hasOwnProperty.call(res, "data")
+        || !Object.hasOwnProperty.call(res.data, "product")) {
         _logger.info('product query failed', JSON.stringify(res));
-        return null;
+        return "Product query failed";
       };
-      const products = res.products;
-      if (products.length === 0) {
-        _logger.info('no product found on shop');
-        return null;
-      } else if (products.length > 1) {
-        _logger.info('more than one product found on shop');
-        return null;
+      const product = res.data.product;
+      const tag = product.tags.map(el => el.trim()).find(el => tags.includes(el));
+      if (product.sellingPlanGroupCount === 0) {
+        return 'No selling plan for this product';
+      } else if (product.sellingPlanGroups.nodes[0].options[0] !== "1 Week(s), 2 Week(s)") {
+        return `Incorrect options on selling plan ${product.sellingPlanGroups.nodes[0].options[0]}`;
+      } else if (product.productType !== "Box Produce") {
+        return `Incorrect product type: ${product.productType}`;
+      } else if (product.variants.nodes.length > 1) {
+        return "Multiple variants found for box produce";
+      } else if (product.variants.nodes[0].title !== "Default Title") {
+        return "Box produce may not have options";
+      } else if (!Boolean(tag)) {
+        return "Unable to match a tag for the product";
       } else {
-        return makeDoc(products[0]);
-        // insert product to the array
+        product.tag = tag;
+        return makeDoc(product);
       }
     });
 
-  if (productDoc) {
+  if (typeof productDoc === "string") {
+    res.status(400).json({error: productDoc});
 
+  } else {
     const collection = _mongodb.collection("boxes");
     const result = await collection.updateOne(
       { _id: ObjectID(box_id) },
@@ -79,7 +106,5 @@ export default async (req, res, next) => {
     );
 
     res.status(200).json(result);
-  } else {
-    res.status(400).json({error: "Unable to add product"});
   }
 };

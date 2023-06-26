f651c82c2d5fc53eb2cb2fffadb950e5d47e8328
Darryl Cousins <darryljcousins@gmail.com>
2023-06-27 11:38:04 +1200

    Updated order processed webhook to reduce api calls to recharge, improved log metadata.

--- server/src/webhooks/recharge/order-processed.js
+++ server/src/webhooks/recharge/order-processed.js
@@ -21,28 +21,52 @@ export default async function orderProcessed(topic, shop, body) {
   if (order.type === "checkout") return; // only process those "recurring"
 
   try {
-    // need to loop through the line_items and updated the subscription properties to new delivery date
-    // try getting the charge and using those line_items? But I found that the
-    // line_items of a processed order of type "recurring" does not include the properties
-    // So I can only go through each line_item - collect and update the subscription
-    for (const id of order.line_items.map(el => el.purchase_item_id)) {
-      const subscription = await getSubscription(id, 500); // delay each to avoid pushing too many calls
-      const days = parseInt(subscription.order_interval_frequency) * 7; // number of weeks by 7
-      const properties = [ ...subscription.properties ];
+    let parent = null; // hang on the box subscription for logging
+    let delivery; // updated delivery date as string
+    let currentDeliveryDate; // the current date for logging
+
+    // loop line_items and find the parent box subscription and calculate new delivery date
+    for (const line_item of order.line_items) {
+
+      if (line_item.properties.some(el => el.name === "Including")) {
+        // get the subscription so as to access order_interval_frequency
+        const subscription = await getSubscription(line_item.purchase_item_id);
+        const days = parseInt(subscription.order_interval_frequency) * 7; // number of weeks by 7
+
+        parent = line_item; // save parent line_item for logging
+        const properties = [ ...parent.properties ];
+        const dateItem = properties.find(el => el.name === "Delivery Date");
+        currentDeliveryDate = dateItem.value;
+        const dateObj = new Date(Date.parse(currentDeliveryDate));
+        dateObj.setDate(dateObj.getDate() + days);
+        delivery = dateObj.toDateString();
+        break;
+      } else {
+        continue;
+      };
+    };
+
+    // loop again to make updates to delivery date
+    for (const line_item of order.line_items) {
+
+      const properties = [ ...line_item.properties ];
       const dateItem = properties.find(el => el.name === "Delivery Date");
       const dateIdx = properties.indexOf(dateItem);
-      const dateObj = new Date(Date.parse(dateItem.value));
-      dateObj.setDate(dateObj.getDate() + days);
-      dateItem.value = dateObj.toDateString();
+      dateItem.value = delivery;
       properties[dateIdx] = dateItem;
-      const result = await updateSubscription(id, { properties }, 500);
+      updateSubscription(line_item.purchase_item_id, { properties });
+
     };
     const meta = {
       recharge: {
         topic: topicLower,
         charge_id: order.charge.id,
-        address_id: order.address_id,
+        subscription_id: parent.purchase_item_id,
         customer_id: order.customer.id,
+        address_id: order.address_id,
+        box: `${parent.title} - ${parent.variant_title}`,
+        delivered: currentDeliveryDate,
+        next_delivery: delivery,
         email: order.customer.email,
         shopify_order_id: order.external_order_id.ecommerce,
         type: order.type,
@@ -50,10 +74,10 @@ export default async function orderProcessed(topic, shop, body) {
     };
     _logger.notice(`Recharge webhook ${topicLower} received and delivery date updated.`, { meta });
 
-    // now should be able to figure the next delivery date and update properties of all the line_items (i.e. subscriptions)
   } catch(err) {
     _logger.error({message: err.message, level: err.level, stack: err.stack, meta: err});
   };
+  return;
 };
 
 

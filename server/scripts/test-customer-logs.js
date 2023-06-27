4a35e3a1c3f279640fd8ff372433d374bea09024
Darryl Cousins <darryljcousins@gmail.com>
2023-06-28 11:30:07 +1200

    Altered test script for collecting customer logs

--- server/scripts/test-customer-logs.js
+++ server/scripts/test-customer-logs.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import { MongoClient, ObjectID } from "mongodb";
 import { getMongoConnection, MongoStore } from "../src/lib/mongo/mongo.js";
+import { sortObjectArrayByKey } from "../src/lib/helpers.js";
 
 global._filename = (_meta) => _meta.url.split("/").pop();
 dotenv.config({ path: path.resolve(_filename(import.meta), '../.env') });
@@ -11,7 +12,7 @@ global._mongodb;
 _logger.notice = (e) => console.log(e);
 
 /**
- * Simple template for node script
+ * Develop a pipeline to get subscription logs for customer
  */
 
 const run = async () => {
@@ -19,84 +20,34 @@ const run = async () => {
   global._mongodb = await getMongoConnection(); // if mongo connection required
 
   const collection = _mongodb.collection("logs");
-
+  const customer_id = 84185810;
+  const subscription_id = 381406130;
   try {
-    const dateStr = "2023-06-24";
-
-    const now = new Date(new Date(dateStr).toISOString().split('T')[0]);
-    console.log(now);
-    console.log(now.getTime());
-    console.log(new Date(now.getTime()));
-
-    const timestamp = now.getTime();
-
-    const today = new Date(timestamp);
-    const yesterday = new Date(timestamp);
-    const tomorrow = new Date(timestamp);
-    yesterday.setDate(today.getDate() - 1);
-    tomorrow.setDate(today.getDate() + 2);
-    console.log(yesterday);
-    console.log(today);
-    console.log(tomorrow);
-
-    let level = "notice";
-    let object = null;
 
     const query = {};
-    //if (level && level !== "all") query.level = level;
-    if (object) query[`meta.${object}`] = { "$exists": true };
-    query["$and"] = [
-      {timestamp: { "$gt": yesterday }},
-      {timestamp: { "$lte": tomorrow }},
-    ];
+    query[`meta.recharge.customer_id`] = parseInt(customer_id);
+    query[`meta.recharge.subscription_id`] = parseInt(subscription_id);
 
     const pipeline = [
       { "$match": query },
-      { "$group": {
-        _id : { $dateToString: { format: "%Y-%m-%d", date: "$timestamp" } },
-        "count": { "$sum": 1 },
-      }},
-      { "$sort": { _id: 1 }},
+      { "$sort": { timestamp: -1 }},
     ];
 
-    console.log('this ran');
     const result = await collection.aggregate(pipeline).toArray();
-    const final = result.reduce(
-      (acc, curr) => Object.assign(acc, { [`${curr._id}`]: curr.count }),
-      {});
-    console.log(final);
 
-    // update tomorrow and the query to get results for the day
-    tomorrow.setDate(tomorrow.getDate() - 1);
-    query["$and"] = [
-      {timestamp: { "$gt": today }},
-      {timestamp: { "$lt": tomorrow }},
-    ];
-    const logs = await collection.find(query).sort({ timestamp: -1 }).toArray();
-    console.log(logs.length);
-
-    // compile the response
-    const current= today.toISOString().split("T")[0];
-    const previous= yesterday.toISOString().split("T")[0];
-    const next= tomorrow.toISOString().split("T")[0];
-    console.log(previous, current, next);
-
-    const response = {};
-    response.previous = { date: previous, count: 0 };
-    if (Object.keys(final).includes(previous)) {
-      response.previous.count = final[previous];
+    // not to many queries are expected so just aggregate in a loop
+    // should be smart enough to add this to the pipeline
+    for (const item of [ ...result ]) {
+      if (item.meta.recharge.shopify_order_id) {
+        const res = await collection.find({"meta.order.shopify_order_id": parseInt(item.meta.recharge.shopify_order_id) }).toArray();
+        for (const item of res) {
+          result.push(item);
+        };
+      }
     };
 
-    response.current = { date: current, count: 0, logs: logs };
-    if (Object.keys(final).includes(current)) {
-      response.current.count = final[current];
-    };
-
-    response.next = { date: next, count: 0 };
-    if (Object.keys(final).includes(next)) {
-      response.next.count = final[next];
-    };
-    console.log(response);
+    const final = sortObjectArrayByKey(result, "timestamp");
+    console.log(final.reverse());
 
   } catch(e) {
     console.error(e);
@@ -110,8 +61,3 @@ const main = async () => {
 };
 
 main().catch(console.error);
-
-
-
-
-

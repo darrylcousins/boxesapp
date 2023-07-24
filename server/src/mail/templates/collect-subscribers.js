dcc61f995d83cf63af9071fc16b76e794e494dc5
Darryl Cousins <darryljcousins@gmail.com>
2023-07-24 15:45:10 +1200

    tidy mail template for collect script

--- server/src/mail/templates/collect-subscribers.js
+++ server/src/mail/templates/collect-subscribers.js
@@ -8,91 +8,91 @@ export default `
       <p style="color:#666;padding:5px 0;margin:0">
         The service that does some of the work has updated the local database of subscribers.
       </p>
-      <ul style="padding-bottom: 10px; padding-top: 10px">
-        <li style="list-style: none">
-          <dl style="
-            border: 1px solid grey;
-            padding: 0.5em;
-          ">
-            <dt style="
-              float: left;
-              clear: left;
-              width: 330px;
-              text-align: right;
-              font-weight: bold;
-              color: grey;
-            ">Existing count</dt>
-            <dd style="
-              margin: 0 0 0 350px;
-              padding: 0 0 0.5em 0;
-            ">{{ existingCount }}</dd>
-            <dt style="
-              float: left;
-              clear: left;
-              width: 330px;
-              text-align: right;
-              font-weight: bold;
-              color: grey;
-            ">Updated count</dt>
-            <dd style="
-              margin: 0 0 0 350px;
-              padding: 0 0 0.5em 0;
-            ">{{ updatedCount }}</dd>
-            <dt style="
-              float: left;
-              clear: left;
-              width: 330px;
-              text-align: right;
-              font-weight: bold;
-              color: grey;
-            ">Subscribers with active subscriptions</dt>
-            <dd style="
-              margin: 0 0 0 350px;
-              padding: 0 0 0.5em 0;
-            ">{{ activeCount }}</dd>
-            <dt style="
-              float: left;
-              clear: left;
-              width: 330px;
-              text-align: right;
-              font-weight: bold;
-              color: grey;
-            ">Subscribers without active subscriptions</dt>
-            <dd style="
-              margin: 0 0 0 350px;
-              padding: 0 0 0.5em 0;
-            ">{{ inactiveCount }}</dd>
-          </dl>
-        </li>
-      </ul>
+      <dl style="
+        margin: 0px;
+        border: 1px solid grey
+        padding: 0.5em;
+      ">
+        <dt style="
+          padding: 0px;
+          margin: 0px;
+          float: left;
+          clear: left;
+          width: 270px;
+          text-align: right;
+          font-weight: bold;
+          color: grey;
+        ">Existing count</dt>
+        <dd style="
+          margin: 0 0 0 280px;
+          padding: 0 0 0.5em 0;
+        ">{{ existingCount }}</dd>
+        <dt style="
+          padding: 0px;
+          margin: 0px;
+          float: left;
+          clear: left;
+          width: 270px;
+          text-align: right;
+          font-weight: bold;
+          color: grey;
+        ">Updated count</dt>
+        <dd style="
+          margin: 0 0 0 280px;
+          padding: 0 0 0.5em 0;
+        ">{{ updatedCount }}</dd>
+        <dt style="
+          padding: 0px;
+          margin: 0px;
+          float: left;
+          clear: left;
+          width: 270px;
+          text-align: right;
+          font-weight: bold;
+          color: grey;
+        ">Subscribers with active subscriptions</dt>
+        <dd style="
+          margin: 0 0 0 280px;
+          padding: 0 0 0.5em 0;
+        ">{{ activeCount }}</dd>
+        <dt style="
+          padding: 0px;
+          margin: 0px;
+          float: left;
+          clear: left;
+          width: 270px;
+          text-align: right;
+          font-weight: bold;
+          color: grey;
+        ">Subscribers without active subscriptions</dt>
+        <dd style="
+          margin: 0 0 0 280px;
+          padding: 0 0 0.5em 0;
+        ">{{ inactiveCount }}</dd>
+      </dl>
       {% if activeNoChargeCount > 0 %}
         <p>
           Subscribers with active subscriptions but no upcoming charges.
           This may mean there was a problem with payment. Their Recharge id is listed below.
         </p>
-        <ul style="padding-bottom: 10px; padding-top: 10px">
-          <li style="list-style: none">
-            <dl style="
-              border: 1px solid grey;
-              padding: 0.5em;
-            ">
-            {% for customer in activeNoCharges %}
-              <dt style="
-                float: left;
-                clear: left;
-                width: 330px;
-                text-align: right;
-                font-weight: bold;
-                color: grey;
-              ">{{ customer.name }}</dt>
-              <dd style="
-                margin: 0 0 0 350px;
-                padding: 0 0 0.5em 0;
-              ">{{ customer.recharge_id }}</dd>
-            {% endfor %}
-            </dl>
-          </li>
-        </ul>
+        <dl style="
+          margin: 0px;
+          border: 1px solid grey
+          padding: 0.5em;
+        ">
+          {% for customer in activeNoCharges %}
+            <dt style="
+              padding: 0px;
+              margin: 0px;
+              font-weight: bold;
+              color: grey;
+            ">{{ customer.name }}</dt>
+            <dd style="
+              text-align: right;
+              padding: 0 2em 0.5em 0;
+            ">{{ customer.recharge_id }}</dd>
+          {% endfor %}
+        </dl>
       {% endif %}
     </mj-text>
   </mj-column>

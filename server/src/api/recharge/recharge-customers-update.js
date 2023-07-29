c6e12005048a1a55bca07e92ef3b6a5c9ad2409b
Darryl Cousins <darryljcousins@gmail.com>
2023-07-29 13:32:38 +1200

    Update local db for single customer - re-sync from admin customers

--- server/src/api/recharge/recharge-customers-update.js
+++ server/src/api/recharge/recharge-customers-update.js
@@ -15,63 +15,56 @@ import { makeRechargeQuery } from "../../lib/recharge/helpers.js";
 
 export default async (req, res, next) => {
 
-  const collection = _mongodb.collection("customers");
+  const { recharge_id } = req.params;
+
   try {
-    const result = await makeRechargeQuery({
-      path: `customers`,
-      query: [
-        ["limit", 250 ],
-      ]
+    const { customer } = await makeRechargeQuery({
+      path: `customers/${recharge_id}`,
     });
-    const { customers, next_cursor, previous_cursor } = result;
 
-    if (next_cursor) {
-      console.log(`Collect customers, got more than 250 ${customers.length}`);
-      // XXX must code for this now while I still can
+    if (!customer) {
+      return res.status(200).json({ error: "Not found" });
     };
 
-    for (const el of customers) {
+    const charge_list = [];
 
-      const charge_list = [];
+    if (customer.subscriptions_active_count > 0) {
+      try {
+        const res = await makeRechargeQuery({
+          path: `charges`,
+          query: [
+            ["customer_id", customer.id ],
+            ["status", "queued" ],
+            ["sort_by", "scheduled_at-asc" ],
+          ]
+        });
 
-      if (el.subscriptions_active_count > 0) {
-        try {
-          const res = await makeRechargeQuery({
-            path: `charges`,
-            query: [
-              ["customer_id", el.id ],
-              ["status", "queued" ],
-              ["sort_by", "scheduled_at-asc" ],
-            ]
-          });
-
-          if (res.charges) {
-            for (const c of res.charges) {
-              console.log(c.id, c.scheduled_at, c.status);
-              charge_list.push([c.id, c.scheduled_at]);
-            };
+        if (res.charges) {
+          for (const c of res.charges) {
+            charge_list.push([c.id, c.scheduled_at]);
           };
-        } catch(err) {
-          _logger.error({message: err.message, level: err.level, stack: err.stack, meta: err});
         };
+      } catch(err) {
+        _logger.error({message: err.message, level: err.level, stack: err.stack, meta: err});
       };
-
-      const doc = {
-        first_name: el.first_name,
-        last_name: el.last_name,
-        email: el.email,
-        recharge_id: el.id,
-        shopify_id: parseInt(el.external_customer_id.ecommerce),
-        subscriptions_active_count: el.subscriptions_active_count,
-        subscriptions_total_count: el.subscriptions_total_count,
-        charge_list,
-      };
-      const result = await collection.updateOne(
-        { recharge_id: parseInt(doc.recharge_id) },
-        { "$set" : doc },
-        { "upsert": true }
-      );
     };
+
+    const doc = {
+      first_name: customer.first_name,
+      last_name: customer.last_name,
+      email: customer.email,
+      recharge_id: customer.id,
+      shopify_id: parseInt(customer.external_customer_id.ecommerce),
+      subscriptions_active_count: customer.subscriptions_active_count,
+      subscriptions_total_count: customer.subscriptions_total_count,
+      charge_list,
+    };
+    const collection = _mongodb.collection("customers");
+    const result = await collection.updateOne(
+      { recharge_id: parseInt(doc.recharge_id) },
+      { "$set" : doc },
+      { "upsert": true }
+    );
 
     res.status(200).json({ success: true });
   } catch(err) {

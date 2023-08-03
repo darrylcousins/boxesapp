afae951fd3ac239a56e3b26316388a271a999577
Darryl Cousins <darryljcousins@gmail.com>
2023-08-03 23:43:05 +1200

    another svg icon

--- server/src/resources/admin/components/lib/icon.jsx
+++ server/src/resources/admin/components/lib/icon.jsx
@@ -36,6 +36,19 @@ const Icon = ({ children, styleSize }) => {
 };
 
 /**
+ * DragIcon component, path borrowed from
+ * {@link https://material.io/icons/|Material Design}. Intended to be wrapped by
+ * {@link module:app/lib/icon~Icon|Icon}
+ *
+ * @returns {Element} DOM component
+ */
+const DragIcon = () => (
+  <Icon>
+    <path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
+  </Icon>
+);
+
+/**
  * SearchIcon component, path borrowed from
  * {@link https://material.io/icons/|Material Design}. Intended to be wrapped by
  * {@link module:app/lib/icon~Icon|Icon}
@@ -404,4 +417,5 @@ export {
   SearchIcon,
   ClearSearchIcon,
   SyncIcon,
+  DragIcon,
 };

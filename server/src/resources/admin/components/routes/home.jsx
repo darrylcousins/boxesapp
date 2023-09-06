5cebf83de7bcf0d7c4b6fcf107bb624973d46d74
Darryl Cousins <darryljcousins@gmail.com>
2023-09-06 14:26:08 +1200

    Removed older test action from home

--- server/src/resources/admin/components/routes/home.jsx
+++ server/src/resources/admin/components/routes/home.jsx
@@ -9,46 +9,6 @@ export default () => {
 
   const host = `https://${ window.location.host }`;
 
-  const saveConfirmation = async () => {
-
-    const sessionId = Math.random().toString(36).substr(2, 9);
-
-    const socket = io(host, {
-      autoConnect: true, // could also do this with socket.open()
-      path: `${proxy}/socket-io`,
-      transports: ["polling"], // disable websocket polling - no wss on shopify
-    });
-
-    socket.emit('connectInit', sessionId);
-
-    socket.on('uploadProgress', (data) => {
-      console.log(data);
-      // display data or update timer
-    });
-
-    socket.on('finished', (id) => {
-      if (id === sessionId) {
-        console.log('closing connection for id', id);
-        socket.disconnect();
-      };
-    });
-
-    socket.on('connected', async (id) => {
-      if (id === sessionId) {
-        console.log('connected with id', id);
-      };
-    });
-
-    socket.on('connect', async () => {
-      console.log("connection opened with id", sessionId);
-    });
-    socket.on('disconnect', async () => {
-      console.log("connection closed with id", sessionId);
-    });
-  };
-
-  //saveConfirmation();
-
   return (
     <div class="w-100 pb2">
       <div class="pl5 w-100">
